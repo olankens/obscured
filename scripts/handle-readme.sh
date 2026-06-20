7a325a9afcd540dfe09de9c6b00f2942ef6a6c0c
olankens <olankens@users.noreply.github.com>
2026-06-20 22:26:39 +0200

    chore: use nine per line

--- scripts/handle-readme.sh
+++ scripts/handle-readme.sh
@@ -10,10 +10,10 @@ ALL=("$SRC"/thumbnail*.avif)
 MAX=${#ALL[@]}
 TXT=""
 for NUM in "${!ALL[@]}"; do
-	ALN=$([ "$NUM" -lt $((MAX - (MAX % 7 == 0 ? 7 : MAX % 7))) ] && echo ' align="center"' || echo '')
+	ALN=$([ "$NUM" -lt $((MAX - (MAX % 9 == 0 ? 9 : MAX % 9))) ] && echo ' align="center"' || echo '')
 	BNM=$(basename "${ALL[$NUM]}" | sed "s/^thumbnail-//")
 	FOL="${BNM%.avif}"
-	TXT="${TXT}<a href=\"source/${FOL}/${BNM}\"><img src=\".assets/$(basename "${ALL[$NUM]}")\"${ALN} width=\"14.2857142857%\"></a>"
+	TXT="${TXT}<a href=\"source/${FOL}/${BNM}\"><img src=\".assets/$(basename "${ALL[$NUM]}")\"${ALN} width=\"11.1111111111%\"></a>"
 done
 awk -v BLK="<p>${TXT}</p>" '
 	/<!-- START_BLOCK -->/ { print; print BLK; skip=1; next }
