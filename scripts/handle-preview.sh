d5debaf95d56787f24ed8a6794f01b194d96cb89
olankens <olankens@users.noreply.github.com>
2026-06-22 09:35:37 +0200

    fix: normalize color profile in preview and thumbnail generation

--- scripts/handle-preview.sh
+++ scripts/handle-preview.sh
@@ -12,6 +12,7 @@ for NUM in "${!ALL[@]}"; do
 	PNG="${ALL[$NUM]}"
 	COL=$([ $(((NUM / 8) % 2)) -eq 0 ] && { [ $((NUM % 2)) -eq 0 ] && echo "#bae6fd" || echo "#7dd3fc"; } || { [ $((NUM % 2)) -eq 0 ] && echo "#7dd3fc" || echo "#bae6fd"; }) # Sky 200 / Sky 300
 	magick "$PNG" \
+		-strip \
 		-resize 80x80! \
 		-bordercolor "$COL" \
 		-border 24x12 \
--- scripts/handle-thumbnails.sh
+++ scripts/handle-thumbnails.sh
@@ -13,5 +13,5 @@ for NUM in "${!ALL[@]}"; do
 	BNM="$(basename "$PNG" .png)"
 	[ -e "$OUT/thumbnail-${BNM}.avif" ] && continue
 	COL=$([ $(((NUM / 8) % 2)) -eq 0 ] && { [ $((NUM % 2)) -eq 0 ] && echo "#bae6fd" || echo "#7dd3fc"; } || { [ $((NUM % 2)) -eq 0 ] && echo "#7dd3fc" || echo "#bae6fd"; }) # Sky 200 / Sky 300
-	magick "$PNG" -resize 80x80! -bordercolor "$COL" -border 24x12 png:- | avifenc --stdin --input-format png "$OUT/thumbnail-${BNM}.avif"
+	magick "$PNG" -resize 80x80! -bordercolor "$COL" -border 24x12 -strip png:- | avifenc --stdin --input-format png "$OUT/thumbnail-${BNM}.avif"
 done
