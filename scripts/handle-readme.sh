5c3447272172201cd7a0293c0fa03aa4ec3e658c
olankens <olankens@users.noreply.github.com>
2026-06-22 09:24:53 +0200

    chore: make collection looks like preview

--- scripts/handle-readme.sh
+++ scripts/handle-readme.sh
@@ -10,10 +10,10 @@ ALL=("$SRC"/thumbnail*.avif)
 MAX=${#ALL[@]}
 TXT=""
 for NUM in "${!ALL[@]}"; do
-	ALN=$([ "$NUM" -lt $((MAX - (MAX % 9 == 0 ? 9 : MAX % 9))) ] && echo ' align="center"' || echo '')
+	ALN=$([ "$NUM" -lt $((MAX - (MAX % 8 == 0 ? 8 : MAX % 8))) ] && echo ' align="center"' || echo '')
 	BNM=$(basename "${ALL[$NUM]}" | sed "s/^thumbnail-//")
 	FOL="${BNM%.avif}"
-	TXT="${TXT}<a href=\"source/${FOL}/${BNM%%.*}.icns\"><img src=\".assets/$(basename "${ALL[$NUM]}")\"${ALN} width=\"11.1111111111%\"></a>"
+	TXT="${TXT}<a href=\"source/${FOL}/${BNM%%.*}.icns\"><img src=\".assets/$(basename "${ALL[$NUM]}")\"${ALN} width=\"12.5%\"></a>"
 done
 awk -v BLK="<p>${TXT}</p>" '
 	/<!-- START_BLOCK -->/ { print; print BLK; skip=1; next }
--- scripts/handle-thumbnails.sh
+++ scripts/handle-thumbnails.sh
@@ -12,6 +12,6 @@ for NUM in "${!ALL[@]}"; do
 	[ -e "$PNG" ] || continue
 	BNM="$(basename "$PNG" .png)"
 	[ -e "$OUT/thumbnail-${BNM}.avif" ] && continue
-	COL=$([ $((NUM % 2)) -eq 0 ] && echo "#e2e8f0" || echo "#cbd5e1") # Slate 200 / Slate 300
-	magick "$PNG" -bordercolor "$COL" -border 144 png:- | avifenc --stdin --input-format png "$OUT/thumbnail-${BNM}.avif"
+	COL=$([ $(((NUM / 8) % 2)) -eq 0 ] && { [ $((NUM % 2)) -eq 0 ] && echo "#bae6fd" || echo "#7dd3fc"; } || { [ $((NUM % 2)) -eq 0 ] && echo "#7dd3fc" || echo "#bae6fd"; }) # Sky 200 / Sky 300
+	magick "$PNG" -resize 80x80! -bordercolor "$COL" -border 24x12 png:- | avifenc --stdin --input-format png "$OUT/thumbnail-${BNM}.avif"
 done
