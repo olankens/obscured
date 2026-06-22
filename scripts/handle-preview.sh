5cf72f4de2b72aaaf452ed407449d14084521137
olankens <olankens@users.noreply.github.com>
2026-06-22 12:05:52 +0200

    chore: remove comments from handle-preview.sh

--- scripts/handle-preview.sh
+++ scripts/handle-preview.sh
@@ -7,7 +7,7 @@ SRC="$DIR/../source"
 OUT="$DIR/../.assets/preview-01.avif"
 TMP="$(mktemp -d)"
 
-mapfile -t ALL < <(find "$SRC" -maxdepth 2 -name "*.png" | sort)
+mapfile -t ALL < <(find "$SRC" -maxdepth 2 -name "*.png" | sort | head -n 32)
 for NUM in "${!ALL[@]}"; do
 	PNG="${ALL[$NUM]}"
 	COL=$([ $(((NUM / 8) % 2)) -eq 0 ] && { [ $((NUM % 2)) -eq 0 ] && echo "#bae6fd" || echo "#7dd3fc"; } || { [ $((NUM % 2)) -eq 0 ] && echo "#7dd3fc" || echo "#bae6fd"; }) # Sky 200 / Sky 300
@@ -18,7 +18,6 @@ for NUM in "${!ALL[@]}"; do
 		-border 24x12 \
 		"$TMP/$(basename "$PNG")"
 done
-#magick montage "$TMP"/*.png -tile 8x4 -geometry +0+0 "$OUT"
-magick montage "$TMP"/*.png -tile 8x4 -geometry +0+0 png:- | avifenc --stdin --input-format png "$OUT"
 
+magick montage "$TMP"/*.png -tile 8x4 -geometry +0+0 png:- | avifenc --stdin --input-format png "$OUT"
 echo "Generated $OUT"
