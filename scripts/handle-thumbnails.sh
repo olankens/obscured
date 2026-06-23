2c5d2d501f3eb80fd752652024191486519d35ac
olankens <olankens@users.noreply.github.com>
2026-06-23 11:20:59 +0200

    fix: always regenerate the thumbnails

--- scripts/handle-thumbnails.sh
+++ scripts/handle-thumbnails.sh
@@ -11,7 +11,6 @@ for NUM in "${!ALL[@]}"; do
 	PNG="${ALL[$NUM]}"
 	[ -e "$PNG" ] || continue
 	BNM="$(basename "$PNG" .png)"
-	[ -e "$OUT/thumbnail-${BNM}.avif" ] && continue
 	COL=$([ $(((NUM / 8) % 2)) -eq 0 ] && { [ $((NUM % 2)) -eq 0 ] && echo "#bae6fd" || echo "#7dd3fc"; } || { [ $((NUM % 2)) -eq 0 ] && echo "#7dd3fc" || echo "#bae6fd"; }) # Sky 200 / Sky 300
 	magick "$PNG" -resize 80x80! -bordercolor "$COL" -border 24x12 -strip png:- | avifenc --stdin --input-format png "$OUT/thumbnail-${BNM}.avif"
 done
