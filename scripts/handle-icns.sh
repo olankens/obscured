bdad2ac8db4e8aff7cdd546bb936e1db7344feb4
Jolan Gerkens <173156207+olankens@users.noreply.github.com>
2026-09-23 23:13:17 +0200

    feat: add 10% padding to generated icons and regenerate all icns files

--- scripts/handle-icns.sh
+++ scripts/handle-icns.sh
@@ -23,21 +23,43 @@ create_icns() {
 	# Create icon
 	local iconset_dir="$(mktemp -d)/$app_name.iconset"
 	mkdir -p "$iconset_dir"
-	sips -z 16 16 "$app_dir/$app_name.png" --out "$iconset_dir/icon_16x16.png"
-	sips -z 32 32 "$app_dir/$app_name.png" --out "$iconset_dir/icon_16x16@2x.png"
-	sips -z 32 32 "$app_dir/$app_name.png" --out "$iconset_dir/icon_32x32.png"
-	sips -z 64 64 "$app_dir/$app_name.png" --out "$iconset_dir/icon_32x32@2x.png"
-	sips -z 128 128 "$app_dir/$app_name.png" --out "$iconset_dir/icon_128x128.png"
-	sips -z 256 256 "$app_dir/$app_name.png" --out "$iconset_dir/icon_128x128@2x.png"
-	sips -z 256 256 "$app_dir/$app_name.png" --out "$iconset_dir/icon_256x256.png"
-	sips -z 512 512 "$app_dir/$app_name.png" --out "$iconset_dir/icon_256x256@2x.png"
-	sips -z 512 512 "$app_dir/$app_name.png" --out "$iconset_dir/icon_512x512.png"
-	sips -z 1024 1024 "$app_dir/$app_name.png" --out "$iconset_dir/icon_512x512@2x.png"
+	local sizes=(
+		"16 icon_16x16.png"
+		"32 icon_16x16@2x.png"
+		"32 icon_32x32.png"
+		"64 icon_32x32@2x.png"
+		"128 icon_128x128.png"
+		"256 icon_128x128@2x.png"
+		"256 icon_256x256.png"
+		"512 icon_256x256@2x.png"
+		"512 icon_512x512.png"
+		"1024 icon_512x512@2x.png"
+	)
+	for s in "${sizes[@]}"; do
+		read -r size filename <<<"$s"
+		resize_icon "$size" "$filename" "$app_dir/$app_name.png" "$iconset_dir"
+	done
 	iconutil -c icns "$iconset_dir" -o "$app_dir/$app_name.icns"
 
 	# Remove remnants
 	rm -rf "${iconset_dir%/*}"
 	pngquant --force --output "$app_dir/$app_name.png" "$app_dir/$app_name.png"
+
+}
+
+resize_icon() {
+
+	# Handle parameters
+	local target="$1"
+	local filename="$2"
+	local src="$3"
+	local out_dir="$4"
+
+	# Resize icon
+	local content=$((target - 2 * target * 10 / 100))
+	local picture="$(mktemp -t iconresize).png"
+	sips -z "$content" "$content" "$src" --out "$picture" >/dev/null
+	sips -s format png --padToHeightWidth "$target" "$target" "$picture" --out "$out_dir/$filename" >/dev/null
 
 }
 
